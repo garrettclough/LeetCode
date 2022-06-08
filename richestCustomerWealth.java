dfe40f1c555e13280b33d777aa5128515710c672
eggin <garrett@duck.com>
2022-06-08 14:56:02 -0700

    Create richestCustomerWealth.java

---
+++ richestCustomerWealth.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maximumWealth = 0;
+        for (int[] account : accounts) {
+            int customerWealth = 0;
+            for (int dollars : account) {
+                customerWealth += dollars;
+            }
+            maximumWealth = Math.max(maximumWealth, customerWealth);
+        }
+        return maximumWealth;
+    }
+}
